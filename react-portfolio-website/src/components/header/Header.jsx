62e07c53ca5a0e55a7cb704e74ca5a42aeb4c83d
Akshit Thakur <akshit2649@gmail.com>
2022-05-19 13:25:40 +0530

    styled header component

--- react-portfolio-website/src/components/header/Header.jsx
+++ react-portfolio-website/src/components/header/Header.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import CTA from "./CTA";
-import classes from "./Header.module.css";
+import "./Header.css";
 import ME from "../../assets/me.png";
 import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
   return (
     <header>
-      <div className={`container ${classes.header__container}`}>
+      <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Akshit Thakur</h1>
         <h5 className="text-light">Frontend Developer</h5>
