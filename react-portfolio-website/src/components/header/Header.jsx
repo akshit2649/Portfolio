bde3efb12a2136b620cfc6d9058d6df6446997de
Akshit Thakur <akshit2649@gmail.com>
2022-05-19 10:45:47 +0530

    Header Component

--- react-portfolio-website/src/components/header/Header.jsx
+++ react-portfolio-website/src/components/header/Header.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+import CTA from "./CTA";
 import classes from "./Header.module.css";
+import ME from "../../assets/me.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className={`container ${classes.header__container}`}>
+        <h5>Hello I'm</h5>
+        <h1>Akshit Thakur</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+        <CTA />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
