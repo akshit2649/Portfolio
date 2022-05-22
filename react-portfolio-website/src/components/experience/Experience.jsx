6ce68055c271dea127d10da6fa8bc751cf627261
Akshit Thakur <akshit2649@gmail.com>
2022-05-22 19:32:11 +0530

    Experience JSX

--- react-portfolio-website/src/components/experience/Experience.jsx
+++ react-portfolio-website/src/components/experience/Experience.jsx
@@ -1,8 +1,71 @@
 import React from "react";
-import classes from "./Experience.module.css";
+import "./Experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
-  return <section id="experience">Experience</section>;
+  return (
+    <section id="experience">
+      <h5>The skills I have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>HTML</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>CSS</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>JavaScript</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>BootStrap</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>React</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+          </div>
+        </div>
+        <div className="experience__backend"></div>
+        <h3>Backend Development</h3>
+        <div className="experience__content">
+          <article className="experience__details">
+            <BsPatchCheckFill />
+            <h4>Nodejs</h4>
+            <small className="text-light">Experienced</small>
+          </article>
+          <article className="experience__details">
+            <BsPatchCheckFill />
+            <h4>MongoDB</h4>
+            <small className="text-light">Intermediate</small>
+          </article>
+          <article className="experience__details">
+            <BsPatchCheckFill />
+            <h4>Java</h4>
+            <small className="text-light">Experienced</small>
+          </article>
+          <article className="experience__details">
+            <BsPatchCheckFill />
+            <h4>Python</h4>
+            <small className="text-light">Experienced</small>
+          </article>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
