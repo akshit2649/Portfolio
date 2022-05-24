476351899a469734666697ae4923046f19af9729
Akshit Thakur <akshit2649@gmail.com>
2022-05-24 14:08:37 +0530

    Footer JSX

--- react-portfolio-website/src/components/footer/Footer.jsx
+++ react-portfolio-website/src/components/footer/Footer.jsx
@@ -1,8 +1,57 @@
 import React from "react";
-import classes from "./Footer.module.css";
+import "./Footer.css";
+import { FaFacebookF } from "react-icons/fa";
+import { BsInstagram } from "react-icons/bs";
+import { BsTwitter } from "react-icons/bs";
 
 const Footer = () => {
-  return <section id="footer">Footer</section>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        EGATOR
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contacts">Contacts</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com">
+          <FaFacebookF />
+        </a>
+        <a href="https://instagram.com">
+          <BsInstagram />
+        </a>
+        <a href="https://twitter.com">
+          <BsTwitter />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Akshit Thakur. All rights reserved.</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
